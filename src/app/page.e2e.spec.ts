4d8b3c424e01e3e08b3f1b745437f4ac933b4a38
Razvan Negrea <rznart@gmail.com>
2023-09-10 16:12:44 +0200

    Add a simple E2E test

--- src/app/page.e2e.spec.ts
+++ src/app/page.e2e.spec.ts
@@ -1,5 +1,11 @@
 import { test, expect } from "@playwright/test"
 
-test("should navigate to the about page", () => {
-  expect(1 + 1).toBe(2)
+test("should navigate through various page sections", async ({ page }) => {
+  await page.goto("http://localhost:3000/")
+
+  await page.getByRole("link", { name: "Get started" }).click()
+  await expect(page).toHaveURL("http://localhost:3000/#plans")
+
+  await page.getByRole("link", { name: "Discover more" }).click()
+  await expect(page).toHaveURL("http://localhost:3000/#vision")
 })
