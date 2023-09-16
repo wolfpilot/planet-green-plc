1e6f54cdf5fc02ef6f42b91aa3b7034923e89d5d
Razvan Negrea <rznart@gmail.com>
2023-09-16 19:09:21 +0200

    Add unit tests

---
+++ src/components/pages/website/plans/PlanItem.unit.spec.tsx
@@ -0,0 +1,29 @@
+import { screen, render } from "@testing-library/react"
+
+// Data
+import { mockPlans } from "@data/homepage"
+
+// Components
+import PlanItem from "./PlanItem"
+
+describe("PlanItem", () => {
+  it("renders correctly", () => {
+    expect(1 + 1).toBe(2)
+
+    const { container } = render(<PlanItem {...mockPlans.items[0]} />)
+
+    expect(container).toHaveTextContent("€0.28/kWH")
+
+    expect(
+      screen.getByRole("heading", {
+        name: "Fixed costs",
+      })
+    )
+
+    expect(
+      screen.getByText(
+        "Do you prefer having peace of mind? Sign up to our fixed cost plan and receive guaranteed rates (*) for your entire contract length."
+      )
+    )
+  })
+})
